b5d1c728e897137a45f02d4e3e1f5a7fd6387a57
Igor Falendysh <74731862+BlackfyreInc@users.noreply.github.com>
2021-09-26 13:00:28 +0300

    Delete hw2_41e.java

--- hw2/hw2_41e.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class hw2_41e {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt(), x2 = x*x*x, x3 = x2*x*x;
-        System.out.println("y = " + (x + x2+ x3));
-    }
-}
