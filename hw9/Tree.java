6dc2c976ee056637a6cd8cd0fe969d4183a31589
Igor Falendysh <74731862+BlackfyreInc@users.noreply.github.com>
2021-11-06 16:41:51 +0200

    Add files via upload

---
+++ hw9/Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+
+    String n;
+    Branch branch;
+    Leaf leaf;
+    public Tree(String name, Branch a, Leaf b){
+        n=name;
+        branch=a;
+        leaf=b;
+    }
+    public Tree(){}
+}
