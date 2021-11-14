ebde47bf718b31d7d97c92767b396242115e8858
Igor Falendysh <74731862+BlackfyreInc@users.noreply.github.com>
2021-11-14 18:19:00 +0200

    Add files via upload

---
+++ cw5/cw5_1_1.java
@@ -0,0 +1,8 @@
+public class cw5_1_1 {
+    public static void main(String[] args) {
+        String[] str_array = {"a", "b", "c"};
+        for(String str : str_array) {
+            System.out.println(str);
+        }
+    }
+}
